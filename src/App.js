351420647d2993c764094b02d91337f49d0d2496
Paul Rogers <progers999@hotmail.co.uk>
2021-02-15 22:02:49 +0000

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    	<h1>Hello World</h1>
+    	<h2>Hello Another World</h2>
+
+    	<ul>
+    		<li>This is one thing</li>
+    		<li>This is another thing</li>
+    	</ul>
     </div>
   );
 }
