60000059941529e2ff46ad346a175d88301653bb
Paul Rogers <progers999@hotmail.co.uk>
2021-02-16 13:54:59 +0000

    Header and navbar built and styled

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+				<Navbar.Brand href="">Paul Rogers</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+					<Nav>
+						<Nav.Link className="ml-4" href="">Home</Nav.Link>
+						<Nav.Link className="ml-4" href="">Portfolio</Nav.Link>
+						<Nav.Link className="ml-4" href="">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</header>
+	)
+
+}
+
+export default Header
