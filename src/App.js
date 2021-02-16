b8aa66109ec3018e09fbb91e0ee9a66bbf6d4a4c
Paul Rogers <progers999@hotmail.co.uk>
2021-02-16 13:05:43 +0000

    Some assets have been added to the project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-    	<h1>Hello World</h1>
-    	<h2>Hello Another World</h2>
-
-    	<ul>
-    		<li>This is one thing</li>
-    		<li>This is another thing</li>
-    	</ul>
     </div>
   );
 }
