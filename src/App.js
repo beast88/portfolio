af68eb962e4c3db7c73ef09f6f5424e4de095296
Paul Rogers <progers999@hotmail.co.uk>
2021-02-22 12:46:56 +0000

    Latest project section has placeholder styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Components/Header'
 import Hero from './Components/Hero'
+import LatestProjects from './Components/LatestProjects'
+
+import './Styles/App.css'
 
 function App() {
   return (
     <div className="App">
     	<Header />
     	<Hero />
+    	<div className="main-bg">
+    		<LatestProjects />
+    	</div>
     </div>
   );
 }
---
+++ src/Components/LatestProjects.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../Styles/Latest.css'
+
+const LatestProjects = () => {
+	return(
+		<div className="latest-container">
+			<div className="dots-container">
+				<div id="dot-1" className="browser-dot"></div>
+				<div id="dot-2" className="browser-dot"></div>
+				<div id="dot-3" className="browser-dot"></div>
+			</div>
+
+			<h2>Latest Projects</h2>
+		</div>
+	)
+}
+
+export default LatestProjects
