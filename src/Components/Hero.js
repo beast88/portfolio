26f8f1a5dd4ada434540b632690b7e905e9d93b3
Paul Rogers <progers999@hotmail.co.uk>
2021-02-25 13:34:16 +0000

    Color scheme changed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -7,47 +7,49 @@ import images from '../Assets/images'
 const Hero = () => {
 	return(
 		<div className="hero-bg d-flex justify-content-center align-items-start">
-			<div className="hero d-flex flex-column flex-md-row justify-content-around align-items-center">
+			<div className="backdrop">
+				<div className="hero d-flex flex-column flex-md-row justify-content-around align-items-center">
 
-				<div className="intro p-5 mb-4 mb-md-0">
-					<h1>Paul Rogers</h1>
-					<p>Front End Web Developer</p>
+					<div className="intro p-5 mb-4 mb-md-0">
+						<h1>Paul Rogers</h1>
+						<p>Front End Web Developer</p>
+					</div>
+
+					<Carousel className="justify-self-center">
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src={images.html}
+								alt="HTML" 
+							/>
+						</Carousel.Item>
+
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src={images.css}
+								alt="CSS" 
+							/>
+						</Carousel.Item>
+
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src={images.js}
+								alt="Javascript" 
+							/>
+						</Carousel.Item>
+
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src={images.react}
+								alt="React" 
+							/>
+						</Carousel.Item>
+					</Carousel>
+
 				</div>
-
-				<Carousel className="justify-self-center">
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src={images.html}
-							alt="HTML" 
-						/>
-					</Carousel.Item>
-
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src={images.css}
-							alt="CSS" 
-						/>
-					</Carousel.Item>
-
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src={images.js}
-							alt="Javascript" 
-						/>
-					</Carousel.Item>
-
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src={images.react}
-							alt="React" 
-						/>
-					</Carousel.Item>
-				</Carousel>
-
 			</div>
 		</div>
 	)
